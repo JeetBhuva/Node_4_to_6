abdb75b0d874c5f765171770808aa81c4a7b3799
Jeet-Trueline <118980980+JeetBhuva@users.noreply.github.com>
2025-02-14 17:12:05 +0530

    14/02/2025
    getData,insertData,updateData,deleteData

--- index.js
+++ index.js
@@ -29,27 +29,89 @@
 // ).listen(8080)
 
 const express = require('express');
+const { MongoClient, ObjectId } = require('mongodb');
 const calcu = require('./calcu')
 const app = express();
+
+app.use(express.json())
+
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+async function getData() {
+    await client.connect();
+    const db = client.db("NodeAPI");
+    const collection = db.collection('User');
+    const data = await collection.find({}).toArray();
+    return data
+}
+
+async function insertData(data) {
+    await client.connect();
+    const db = client.db("NodeAPI");
+    const collection = db.collection('User');
+    const insertData = await collection.insertOne(data)
+    return insertData
+}
+
+async function updateData(id, data) {
+    await client.connect();
+    const db = client.db("NodeAPI");
+    const collection = db.collection('User');
+    const update = await collection.updateMany({ _id: new ObjectId(id) }, { $set: data });
+    // console.log(update);
+    return update
+}
+
+async function deleteData() {
+    await client.connect();
+    const db = client.db("NodeAPI");
+    const collection = db.collection('User');
+    const deleteData = await collection.deleteOne({ _id: new ObjectId('67a9de9d001253c0240cd954') });
+    console.log(deleteData);
+    // return deleteData
+}
+
+deleteData()
+
 
 app.get('/', (req, res) => {
     res.send("This is my home Page")
 })
 
-app.get('/add', (req, res) => {
-
-    const num1 = req.query.num1;
-    const num2 = req.query.num2;
-
-    const data = calcu.add(num1, num2)
+app.get('/getData', async (req, res) => {
+    const data = await getData()
     res.send(data)
 })
 
-app.get('/sub', (req, res) => {
-    const num1 = req.query.num1;
-    const num2 = req.query.num2;
-    const data = calcu.sub(num1, num2)
+app.post('/insertData', async (req, res) => {
+    const data = await insertData(req.body);
     res.send(data)
 })
+
+app.put('/updateData/:id', async (req, res) => {
+    const data = await updateData(req.params.id, req.body)
+    res.send(data)
+})
+
+
+
+
+
+
+
+// app.get('/add/:num1/:num2', (req, res) => {
+//     const num1 = req.params.num1;
+//     const num2 = req.params.num2;
+//     const data = calcu.add(num1, num2)
+//     res.send(data)
+// })
+
+// app.get('/sub', (req, res) => {
+//     const num1 = req.query.num1;
+//     const num2 = req.query.num2;
+//     const data = calcu.sub(num1, num2)
+//     res.send(data)
+// })
 
 app.listen(8080);
