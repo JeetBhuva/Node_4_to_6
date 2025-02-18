d35412dd80d4e5f448e16461d05522f7c9fa7f6b
Jeet-Trueline <118980980+JeetBhuva@users.noreply.github.com>
2025-02-18 16:43:09 +0530

    18/02/2025
    mongoose

--- index.js
+++ index.js
@@ -29,75 +29,93 @@
 // ).listen(8080)
 
 const express = require('express');
-const { MongoClient, ObjectId } = require('mongodb');
-const calcu = require('./calcu')
+const mongoose = require('mongoose');
+// const { MongoClient, ObjectId } = require('mongodb');
+// const calcu = require('./calcu')
 const app = express();
 
 app.use(express.json())
 
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
+const main = async (req, res) => {
+    await mongoose.connect('mongodb://localhost:27017/Abcd')
 
-async function getData() {
-    await client.connect();
-    const db = client.db("NodeAPI");
-    const collection = db.collection('User');
-    const data = await collection.find({}).toArray();
-    return data
+    const userSchema = mongoose.Schema({
+        name: String,
+        email: String,
+        password: String,
+        mobile: Number
+    })
+
+    const MyModel = mongoose.model('xyz', userSchema);
+
+    const data = new MyModel({ name: "Jeet", email: "jeet@gmail.com", password: "Jeet@123", mobile: 9123456789 })
+
+    const results = await data.save();
+    console.log(results);
+
 }
 
-async function insertData(data) {
-    await client.connect();
-    const db = client.db("NodeAPI");
-    const collection = db.collection('User');
-    const insertData = await collection.insertOne(data)
-    return insertData
-}
-
-async function updateData(id, data) {
-    await client.connect();
-    const db = client.db("NodeAPI");
-    const collection = db.collection('User');
-    const update = await collection.updateMany({ _id: new ObjectId(id) }, { $set: data });
-    // console.log(update);
-    return update
-}
-
-async function deleteData() {
-    await client.connect();
-    const db = client.db("NodeAPI");
-    const collection = db.collection('User');
-    const deleteData = await collection.deleteOne({ _id: new ObjectId('67a9de9d001253c0240cd954') });
-    console.log(deleteData);
-    // return deleteData
-}
-
-deleteData()
+main();
 
 
-app.get('/', (req, res) => {
-    res.send("This is my home Page")
-})
+// const url = 'mongodb://localhost:27017';
+// const client = new MongoClient(url);
 
-app.get('/getData', async (req, res) => {
-    const data = await getData()
-    res.send(data)
-})
+// async function getData() {
+//     await client.connect();
+//     const db = client.db("NodeAPI");
+//     const collection = db.collection('User');
+//     const data = await collection.find({}).toArray();
+//     return data
+// }
 
-app.post('/insertData', async (req, res) => {
-    const data = await insertData(req.body);
-    res.send(data)
-})
+// async function insertData(data) {
+//     await client.connect();
+//     const db = client.db("NodeAPI");
+//     const collection = db.collection('User');
+//     const insertData = await collection.insertOne(data)
+//     return insertData
+// }
 
-app.put('/updateData/:id', async (req, res) => {
-    const data = await updateData(req.params.id, req.body)
-    res.send(data)
-})
+// async function updateData(id, data) {
+//     await client.connect();
+//     const db = client.db("NodeAPI");
+//     const collection = db.collection('User');
+//     const update = await collection.updateMany({ _id: new ObjectId(id) }, { $set: data });
+//     // console.log(update);
+//     return update
+// }
+
+// async function deleteData() {
+//     await client.connect();
+//     const db = client.db("NodeAPI");
+//     const collection = db.collection('User');
+//     const deleteData = await collection.deleteOne({ _id: new ObjectId('67a9de9d001253c0240cd954') });
+//     console.log(deleteData);
+//     // return deleteData
+// }
+
+// deleteData()
 
 
+// app.get('/', (req, res) => {
+//     res.send("This is my home Page")
+// })
 
+// app.get('/getData', async (req, res) => {
+//     const data = await getData()
+//     res.send(data)
+// })
 
+// app.post('/insertData', async (req, res) => {
+//     const data = await insertData(req.body);
+//     res.send(data)
+// })
 
+// app.put('/updateData/:id', async (req, res) => {
+//     const data = await updateData(req.params.id, req.body)
+//     res.send(data)
+// })
 
 
 // app.get('/add/:num1/:num2', (req, res) => {
